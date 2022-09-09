82973387002c13eef898370cd221df2e90c1e8cb
VladlmlrF <vladimirfalin84@gmail.com>
2022-09-09 19:23:55 +0300

    Initial Commit

---
+++ FoodRecipesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodRecipesApp
+//
+//  Created by Владимир Фалин on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
