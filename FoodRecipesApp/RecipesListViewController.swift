1f15926fdeb7d61dc6063c1d8467ba086b94b62e
VladlmlrF <vladimirfalin84@gmail.com>
2022-09-09 19:29:50 +0300

    Delete storyboard and create RecipesListViewController

---
+++ FoodRecipesApp/RecipesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesListViewController.swift
+//  FoodRecipesApp
+//
+//  Created by Владимир Фалин on 09.09.2022.
+//
+
+import UIKit
+
+class RecipesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
